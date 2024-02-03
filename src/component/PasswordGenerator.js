2d1ee779eb73281ee5ccd47aad76431ec669a23a
varshit gupta <varshitguta463@gmail.com>
2024-02-04 01:51:04 +0530

    Basic Project

--- src/component/PasswordGenerator.js
+++ src/component/PasswordGenerator.js
@@ -1,16 +1,36 @@
 import React from 'react'
+import { useEffect , useState } from 'react';
 import "./css/main.css"
 
 function PasswordGenerator() {
 
-    const [length, setLength] =  React.useState(8)
+    const [length, setLength] =  useState(8);
+    const [numAllow , setNumAllow]=useState(false);
+    const [symAllow  ,setSymAllow] = useState(false);
+    const [password  , setPassword ]= useState("");
 
-    const setPassword = () =>{
+    const passwordGenerate = () =>{
         let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-        for (let i=0; i < length; i++) {
-             
+        // console.log(str);
+        let pass = '';
+        if (numAllow) str += "1234567890";
+        if (symAllow) str += "`~!@#$%^&*";
+
+        for (let i=1; i <= length; i++) {
+          let  char = Math.floor(Math.random() * str.length + 1);
+          // console.log(char);
+          pass = pass + str.charAt(char);
         }
-    }
+          // console.log(pass);
+          setPassword(pass);
+        }
+
+     
+    useEffect(() => {
+
+       passwordGenerate();
+
+    },[ length , numAllow  , symAllow , setPassword  ]);
 
 
   return (
@@ -18,15 +38,15 @@ function PasswordGenerator() {
 
     <div  className="container">
     <h2 style={{textAlign:"center"}} >Password Generator</h2>
-    <input type="text" value="" className="password-input" placeholder="Password"/>
+    <input type="text" value={password} className="password-input" placeholder="Password"/>
     <button  className="copy-button">Copy</button>
     <br/>
     <br/>
-    <input type="range" min={6} max={45} value={length} onChange={(e) => setLength(e.target.value)} />
+    <input type="range" min={6} max={35} value={length} onChange={(e) => setLength(e.target.value)} />
     <label>Length: {length}</label>
-    <input type="checkbox" />
+    <input type="checkbox" checked={numAllow} onChange={() => setNumAllow(!numAllow)}/>
     <label htmlFor="numberInput">Numbers</label>
-    <input type="checkbox" />
+    <input type="checkbox" checked={symAllow}  onChange={() => setSymAllow(!symAllow)}/>
     <label htmlFor="characterInput">Characters</label>
     </div>
 
@@ -34,4 +54,4 @@ function PasswordGenerator() {
   )
 }
 
-export default PasswordGenerator
+export default PasswordGenerator;
